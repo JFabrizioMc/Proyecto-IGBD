02ffb22eb15bb4982ab487ce81551dfe4ec720dd
alumno informatica <alumno@ciis.usil.edu.pe>
2019-10-29 18:16:30 -0500

    Creando la tabla

--- proyecto/index.php
+++ proyecto/index.php
@@ -43,7 +43,36 @@
             <button class="btn btn-primary my-2 my-sm-0" type="submit">Buscar</button>
           </form>
         </div>
-      </nav>
+      </nav>    
+      <table class="table table-hover table-dark">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Handle</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td colspan="2">Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
 </body>
 
